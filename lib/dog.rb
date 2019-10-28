4a087b9675395a0af451e1195062257db748e73a
kelly r maass <kmaass77627@gmail.com>
2019-10-28 04:51:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,16 +2,16 @@ class Dog
   
   fido = Dog.new 
   
+   def bark
+    puts "Woof!"
+  end 
+  
   def name= (fido)
    @this_dogs_name = fido
   end 
  
   def name 
    @this_dogs_name
-  end 
-  
-  def bark
-    puts "Woof!"
   end 
   
 end 
